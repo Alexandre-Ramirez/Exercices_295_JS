92fecfbd818fe6b3203588f02ddb6fd2c48f1e43
ps78ppi <alexandre.ramirez@eduvaud.ch>
2025-08-19 14:38:33 +0200

    Exercice 1

---
+++ index.mjs
@@ -0,0 +1,25 @@
+const playlist = {
+    nb_tracks : 125,
+    display: function() {
+        return (`this playlist contains ${playlist.nb_tracks} tracks.`);
+    }
+};
+
+console.log(playlist.nb_tracks)
+console.log(playlist.display())
+
+let soustraction = 15
+
+let resultat = playlist.nb_tracks -= soustraction
+
+console.log(resultat)
+console.log(playlist.display())
+
+let addition = 10
+
+let resultat_2 = playlist.nb_tracks += addition
+
+console.log(resultat_2)
+console.log(`Maintanant, j'ai ${playlist.nb_tracks} dans ma playlist`)
+
+
